2bee2b6a0b854ffa651a7ef16af44c2ef43997a9
Dewanshh <73938082+Dewanshh@users.noreply.github.com>
2024-06-22 23:20:17 +0530

    Project Added to Github

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RoutePage from './componenets/RoutePage';
 import './index.css';
 function App() {
   return (
-    <div className='h-scree'>
+    <div className='h-screen'>
       <button type='button'className='bg-brand-blue w-full text-center py-3 px-4 md:px-0 text-xl text-white md:text-2lg cursor-pointer'><p>We are taking applications for Fall '24. <span className='underline underline-offset-2 font-semibold'>Apply today!</span></p></button>
     <Header />
     
